0bdc825fc60115f5e8f15fb25c2c1a418b536118
Lbildzinkas <l.barletta@gmail.com>
2017-07-24 23:27:37 +0100

    First commit

---
+++ BitfinexApiSharp/BitfinexApiSharp.Tests/WebSocket/WebSocketClientTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using WebSocketSharp;
+namespace BitfinexApiSharp.Tests.WebSocket
+{
+    
+    public class WebSocketClientTests
+    {
+        [Fact]
+        public void first()
+        {
+            var serverUrl = "wss://api.bitfinex.com/ws";
+            var ws = new WebSocketSharp.WebSocket(serverUrl);
+
+           
+
+            var webSocketClient = new WebSocketApiClient(ws);
+        }
+    }
+
+    public class WebSocketApiClient
+    {
+        private readonly WebSocketSharp.WebSocket _webSocket;
+
+        public WebSocketApiClient(WebSocketSharp.WebSocket webSocket)
+        {
+            _webSocket = webSocket;
+        }
+    }
+}
