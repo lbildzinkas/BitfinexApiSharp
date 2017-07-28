a0475057d645ebd4aaa06c2ea053512f8de5ac6f
lbildzinkas <l.barletta@gmail.com>
2017-07-28 15:35:09 +0100

    Adding configure await

--- BitfinexApiSharp/BitfinexClientSharp.Example/Program.cs
+++ BitfinexApiSharp/BitfinexClientSharp.Example/Program.cs
@@ -12,6 +12,7 @@ namespace BitfinexClientSharp.Example
         private const int receiveChunkSize = 256;
         private const bool verbose = true;
         private static readonly TimeSpan delay = TimeSpan.FromMilliseconds(30000);
+        private static UTF8Encoding encoder = new UTF8Encoding();
 
         static void Main(string[] args)
         {
@@ -28,8 +29,8 @@ namespace BitfinexClientSharp.Example
             try
             {
                 webSocket = new ClientWebSocket();
-                await webSocket.ConnectAsync(new Uri(uri), CancellationToken.None);
-                await Task.WhenAll(Receive(webSocket), Send(webSocket));
+                await webSocket.ConnectAsync(new Uri(uri), CancellationToken.None).ConfigureAwait(false);
+                await Task.WhenAll(Receive(webSocket), Send(webSocket)).ConfigureAwait(false);
             }
             catch (Exception ex)
             {
@@ -49,16 +50,16 @@ namespace BitfinexClientSharp.Example
                 }
             }
         }
-        static UTF8Encoding encoder = new UTF8Encoding();
+        
 
         private static async Task Send(ClientWebSocket webSocket)
         {
             var buffer = encoder.GetBytes("{\r\n  \"event\": \"subscribe\",\r\n  \"channel\": \"ticker\",\r\n  \"symbol\": \"BTCUSD\"\r\n}");
-            await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None).ConfigureAwait(false);
 
             while (webSocket.State == WebSocketState.Open)
             {
-                LogStatus(false, buffer, buffer.Length);
+                LogStatus(false, buffer);
                 await Task.Delay(delay);
             }
         }
@@ -68,19 +69,19 @@ namespace BitfinexClientSharp.Example
             var buffer = new byte[receiveChunkSize];
             while (webSocket.State == WebSocketState.Open)
             {
-                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None).ConfigureAwait(false);
                 if (result.MessageType == WebSocketMessageType.Close)
                 {
-                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None).ConfigureAwait(false);
                 }
                 else
                 {
-                    LogStatus(true, buffer, result.Count);
+                    LogStatus(true, buffer);
                 }
             }
         }
 
-        private static void LogStatus(bool receiving, byte[] buffer, int length)
+        private static void LogStatus(bool receiving, byte[] buffer)
         {
             lock (consoleLock)
             {
